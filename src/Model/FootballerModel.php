23dbcfb9bee8548890bed2229a0d87a5e75a6600
Andrea Stronati <astronati@vendini.com>
2019-08-24 12:35:52 +0200

    ref #8 Add position and status

--- src/Model/FootballerModel.php
+++ src/Model/FootballerModel.php
@@ -14,6 +14,16 @@ class FootballerModel
      */
     private $number;
 
+    /**
+     * @var string
+     */
+    private $status;
+
+    /**
+     * @var int
+     */
+    private $position;
+
     public function __construct(string $name, string $number = null)
     {
         $this->name = $name;
@@ -28,5 +38,37 @@ class FootballerModel
     public function getNumber(): ?string
     {
         return $this->number;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param string $status
+     */
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPosition(): int
+    {
+        return $this->position;
+    }
+
+    /**
+     * @param int $position
+     */
+    public function setPosition(int $position): void
+    {
+        $this->position = $position;
     }
 }
