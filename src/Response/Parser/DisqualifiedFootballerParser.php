b1e6553b1f4668764b965274da449a419075d015
Andrea Stronati <astronati@vendini.com>
2019-09-11 11:07:59 +0200

    ref #24 Check nullable name for footballers #24

--- src/Response/Parser/DisqualifiedFootballerParser.php
+++ src/Response/Parser/DisqualifiedFootballerParser.php
@@ -6,17 +6,21 @@ use SMRP\Model\FootballerModel;
 
 class DisqualifiedFootballerParser
 {
-    public static function parse($footballer): FootballerModel
+    public static function parse($footballer): ?FootballerModel
     {
         $matches = [];
         preg_match('/^(.*) \((\d+)\)$/', $footballer, $matches);
         if (count($matches)) {
             $footballer = FootballerParser::parse($matches[1]);
-            $footballer->setDisqualificationDays($matches[2]);
+            if ($footballer) {
+                $footballer->setDisqualificationDays($matches[2]);
+            }
         }
         else {
             $footballer = FootballerParser::parse($footballer);
-            $footballer->setDisqualificationDays(1);
+            if ($footballer) {
+                $footballer->setDisqualificationDays(1);
+            }
         }
         return $footballer;
     }
--- src/Response/Parser/FootballerParser.php
+++ src/Response/Parser/FootballerParser.php
@@ -9,14 +9,20 @@ class FootballerParser
     const FOOTBALLER_KEY = 'giocatore';
     const NUMBER_KEY = 'numero-maglia';
 
-    public static function parse($footballerData): FootballerModel
+    public static function parse($footballerData): ?FootballerModel
     {
         switch (gettype($footballerData)) {
             case 'string':
-                return new FootballerModel($footballerData);
+                if (trim($footballerData)) {
+                    return new FootballerModel(trim($footballerData));
+                }
+                break;
             case 'array':
-            default:
-                return new FootballerModel($footballerData[self::FOOTBALLER_KEY], $footballerData[self::NUMBER_KEY]);
+                if (trim($footballerData[self::FOOTBALLER_KEY])) {
+                    return new FootballerModel(trim($footballerData[self::FOOTBALLER_KEY]), $footballerData[self::NUMBER_KEY]);
+                }
+                break;
         }
+        return null;
     }
 }
